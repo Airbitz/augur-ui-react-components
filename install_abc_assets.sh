e9bd2a01d09313009d63b20cda7e23dfec42ad41
Paul Puey <paul@airbitz.co>
2016-09-03 21:41:10 -0700

    install the airbitz-core-js-ui dependencies and do 'nam run prebuild'
    prebuild copies some of the dependencies (bootstrap, jQuery) to the assets/dist folder
    This 'should' fix the missing css/js files when running react-components

--- install_abc_assets.sh
+++ install_abc_assets.sh
@@ -4,4 +4,8 @@
 # here is the hack to put the abc-auth assets into the ./demo folder.
 
 mkdir -p demo/abcui
+cd node_modules/airbitz-core-js-ui
+npm install
+npm run prebuild
+cd ../..
 rsync -avz node_modules/airbitz-core-js-ui/assets demo/abcui/
